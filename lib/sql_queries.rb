fcc97dfca0aad33738347f6b89f9a0dbc4fcf123
Otto Cedeno <otto@ottostacos.com>
2020-02-02 15:54:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,10 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT Projects.category, SUM(Pledges.amount) AS amount_pledged FROM Projects
+  JOIN Pledges ON Projects.id = Pledges.project_id
+  WHERE category = 'music'
+  GROUP BY Projects.category;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
