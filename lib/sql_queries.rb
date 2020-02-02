7a416e00926398664756323822db01b5414a9c67
Otto Cedeno <otto@ottostacos.com>
2020-02-02 16:17:12 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,37 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "Write your SQL query Here"
+  "SELECT Projects.title, SUM(Pledges.amount) FROM projects
+  INNER JOIN Pledges ON Projects.id = Pledges.project_id
+  GROUP BY Projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Write your SQL query Here"
+  "SELECT Users.name, Users.age, SUM(Pledges.amount) FROM users
+  INNER JOIN Pledges ON Users.id = Pledges.user_id
+  GROUP BY Users.name ORDER BY Users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT Projects.title, SUM(Pledges.amount) - Projects.funding_goal AS  amount_overfunded FROM projects
+  JOIN Pledges ON Projects.id = Pledges.project_id
+  GROUP BY Projects.title HAVING SUM(Pledges.amount) >= funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT Users.name, SUM(Pledges.amount) AS amount_pledged FROM Users
+  JOIN Pledges ON Users.id = Pledges.user_id
+  GROUP BY Users.name ORDER BY SUM(Pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT Projects.category, Pledges.amount AS amount_pledged FROM Projects
+  JOIN Pledges ON Projects.id = Pledges.project_id
+  WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT Projects.category, SUM(Pledges.amount) AS amount_pledged FROM Projects
+  JOIN Pledges ON Projects.id = Pledges.project_id
+  WHERE category = 'books';"
 end
