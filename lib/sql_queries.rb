6dbd40afd4dc5507b79b4ea82ae7eb3f2d0139e9
Otto Cedeno <otto@ottostacos.com>
2020-02-02 15:57:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,8 +30,8 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT Projects.category, Pledges.amount AS amount_pledged FROM Projects
-JOIN Pledges ON Projects.id = Pledges.project_id
-WHERE category = 'music';"
+  JOIN Pledges ON Projects.id = Pledges.project_id
+  WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
